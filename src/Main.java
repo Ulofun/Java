a2e5911a160e5f78968596f0a592eda0c0c5c0f4
Ulofun <107746667+Ulofun@users.noreply.github.com>
2023-03-17 13:27:05 +0300

    edited

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
-import lesson.Coffee;
-import lesson.Milk;
-import lesson.Task_Lesson;
+
+import secondsemhomework.TaskOneHomeWork;
+import secondsemhomework.TaskTwoHomeWork;
+import sem2practic.Sbpractic;
+import sem2practic.Task1;
 import tasks.Task;
 
 import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    Task task = new Task();
+   // Task task = new Task();
+   // Task1 taskSm = new Task1();
+    TaskOneHomeWork hw1 = new TaskOneHomeWork();
+    TaskTwoHomeWork fourthTask = new TaskTwoHomeWork();
 
-    task.task1();
-    task.task2();
-    task.task3();
+
+//    task.task1();
+//    task.task2();
+//    task.task3();
+ //   taskSm.tasksem1(10, 'x', 'y');
+    hw1.task1hw("ШАЛАШ");
+    fourthTask.writer();
+
+
   }
 }
