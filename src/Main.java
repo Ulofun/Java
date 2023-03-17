961e98d50bf31e314b86bc7a08cde7c8f965bdbf
Ulofun <107746667+Ulofun@users.noreply.github.com>
2023-03-17 13:31:08 +0300

    edited

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-   // Task task = new Task();
-   // Task1 taskSm = new Task1();
+    // Task task = new Task();
+    // Task1 taskSm = new Task1();
     TaskOneHomeWork hw1 = new TaskOneHomeWork();
     TaskTwoHomeWork fourthTask = new TaskTwoHomeWork();
 
@@ -18,10 +18,8 @@ public class Main {
 //    task.task1();
 //    task.task2();
 //    task.task3();
- //   taskSm.tasksem1(10, 'x', 'y');
+    //   taskSm.tasksem1(10, 'x', 'y');
     hw1.task1hw("ШАЛАШ");
     fourthTask.writer();
-
-
   }
 }
